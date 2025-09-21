428389672cd3bf783145f13c8165ff74b7f2bd15
Jay <jay@Jay.localdomain>
2025-09-22 00:00:37 +0300

    Day 7: Python basics and temperature converter challenge

---
+++ Phase1_Rerun/Day07/datatypes_demo.py
@@ -0,0 +1,16 @@
+# datatypes_demo.py
+x = 10
+y = 3.5
+name = "Jay"
+is_devops = True
+numbers = [1, 2, 3, 4]
+profile = {"name": "Jay", "track": "DevOps"}
+
+print(type(x), x)
+print(type(y), y)
+print(type(name), name)
+print(type(is_devops), is_devops)
+print(type(numbers), numbers)
+print(type(profile), profile)
+
+
