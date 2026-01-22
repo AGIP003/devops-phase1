9f5896c37452cbcf0eab323b6c0a52be78588b0d
Agip003 <blessedmuchemi@gmail.com>
2026-01-22 14:36:38 +0300

    Practised and added JSON, OS amd logger functionalities.

--- Phase1_Rerun/Day12/contact_book_challenge.py
+++ Phase1_Rerun/Day12/contact_book_challenge.py
@@ -1,6 +1,59 @@
+import json
+import os
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(name)s -%(message)s" # WHEN | HOW SERIOUS | WHICH ROOM | WHAT HAPPENED 
+)
+
+logger = logging.getLogger(__name__)
+
 class ContactBook:
-    def __init__(self):
-        self.contacts = {}
+    def __init__(self, storage_file="contacts.json"):
+        self.storage_file = storage_file
+        self._ensure_file_exists()
+        self.contacts = self.load_contacts()
+
+    def _ensure_file_exists(self):
+        """Check if file exists and create if it doesn't"""
+        #Data Directory
+        directory = os.path.dirname(self.storage_file)
+        if directory:
+            os.makedirs(directory, exist_ok=True)
+
+        if not os.path.exists(self.storage_file):
+            with open(self.storage_file, "w") as f:
+                json.dump({}, f)
+                logger.info(f"Created New storage file: {self.storage_file}")
+
+    def load_contacts(self):
+        """Load all Contacts"""
+        try:
+            with open(self.storage_file, "r") as f:
+                contacts = json.load(f)
+            logger.debug(f"Loaded {len(contacts)} contacts")
+            return contacts
+        except json.JSONDecodeError as e:
+            logger.error(f"Failed to parse JSON: {e} ")
+            return {}
+        except Exception as e:
+            logger.error(f"Failed to load: {e}")
+            return {}
+
+
+    def save_contacts(self):
+        """Save all contacts"""
+        try:
+            with open(self.storage_file, "w") as f:
+                json.dump(self.contacts, f, indent=4, sort_keys=True)
+                logger.info(f"Saved {len(self.contacts)} successfully.")
+            return True
+        except Exception as e: 
+            logger.error(f"Failed to save contacts: {e}")
+            return False
+
+
 
     def add_contact(self, name, phone, email):
         """Adding a contact to the contactBook"""
@@ -12,7 +65,13 @@ class ContactBook:
                 raise ValueError ("Contact already saved")
             
         self.contacts[name].append({'phone':phone, 'email':email})
+
+        self.save_contacts()
+        logger.info(f"Added contact: {name} - {phone} - {email}")
+
         return f"Added '{name}' to the contact book"
+    
+        
         
     def update_contact(self, name, phone, new_phone=None, new_email=None):
         """Updating a contact in the ContactBook"""
@@ -25,10 +84,16 @@ class ContactBook:
                     contact['phone'] = new_phone
                 if new_email:
                     contact['email'] = new_email
-                
+                self.save_contacts()
+                logger.info(f"Updated contact: {name}")
                 return f"Updated '{name}' succesfully"
-            
-        raise ValueError ("Contact with this phone number not found!!")
+      
+            raise ValueError ("Contact with this phone number not found!!")
+    
+        #self.save_contacts()
+        #logger.info(f"Updated contact: {name}")
+        #return f"Updated '{name}' succesfully"
+
         
     def delete_contact(self, name, phone):
         """Deleting Contacts from the contactbook"""
@@ -42,7 +107,9 @@ class ContactBook:
                 #Remove names if it has no contact
                 if not self.contacts[name]:
                     del self.contacts[name]
-        
+                
+                self.save_contacts()
+                logger.info(f"Deleted contact: {name}")
                 return f"Contact {name} deleted succesfully"
         
         raise ValueError ("Contact with this phone number not found!!")
@@ -88,7 +155,7 @@ class ContactBook:
                 print(f" - {name}:  number - {contact['phone']}, email - {contact['email']}")
 
 # Test Cases
-book = ContactBook()
+book = ContactBook("data/contacts.json")
 book.add_contact("Bob", "555-5678", "bob@email.com")
 book.add_contact("Alice", "555-1234", "alice@email.com")
 book.add_contact("Alice", "2312-32131", "Alice2@gmail.com")
